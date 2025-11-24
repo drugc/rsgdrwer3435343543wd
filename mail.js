1de75ab73d491cff1344020a6b7fdb27e29d14d3
night <mirouout@gmail.com>
2025-11-24 23:50:54 +0100

    Delete mail.js

--- mail.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const email = this.querySelector('input[type="email"]').value;
-    const message = this.querySelector('textarea').value;
-    const mailto = `mailto:mirouout@gmail.com?subject=Contact&body=${encodeURIComponent(`From: ${email}\n\n${message}`)}`;
-    window.location.href = mailto;
-    this.reset();
-});
